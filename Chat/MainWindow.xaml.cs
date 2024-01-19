230cfc207430b3f1536799a482773a298a223f15
Фаниль <fanil724@gmail.com>
2024-01-19 18:40:11 +0300

    Add project files.

---
+++ Chat/MainWindow.xaml.cs
@@ -0,0 +1,168 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Chat
+{
+
+    public partial class MainWindow : Window
+    {
+        ServerObject server;
+        public MainWindow()
+        {
+            server = new ServerObject(this.Dispatcher, this);
+            InitializeComponent();
+            Task.Run(server.ListenAsync);
+        }
+
+
+
+        private void stringAnswer_IsKeyboardFocusWithinChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (stringAnswer.Text == "Введите сообщение") stringAnswer.Text = string.Empty;
+        }
+        private void stringAnswer_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (stringAnswer.Text == string.Empty) stringAnswer.Text = "Введите сообщение";
+        }
+
+        private async void SendAnswer_Click(object sender, RoutedEventArgs e)
+        {
+            if (stringAnswer.Text == string.Empty && stringAnswer.Text == "Введите сообщение")
+            {
+                Chat.Items.Add("Введите сообщение!!!");
+                return;
+            }
+            await server.BroadcastMessageAsync($"Сервер отправил: {stringAnswer.Text}", "");
+            stringAnswer.Text = "Введите сообщение";
+        }
+
+    }
+    public class ServerObject
+    {
+        TcpListener tcplistener = new TcpListener(IPAddress.Any, 888);
+        List<ClientObject> clients = new List<ClientObject>();
+        public Dispatcher dispatcher { get; set; }
+        public MainWindow main { get; set; }
+
+        public ServerObject(Dispatcher dispatcher, MainWindow main)
+        {
+            this.dispatcher = dispatcher;
+            this.main = main;
+        }
+
+        protected internal void RemoveConnetion(string id)
+        {
+            ClientObject? client = clients.FirstOrDefault(x => x.Id == id);
+            if (client != null) clients.Remove(client);
+            client?.Close();
+        }
+        protected internal async Task ListenAsync()
+        {
+            try
+            {
+                tcplistener.Start();
+                dispatcher.Invoke(() => main.Chat.Items.Add("Сервер запущен, Ожидайте подключение...."));
+                while (true)
+                {
+                    TcpClient tcpClient = await tcplistener.AcceptTcpClientAsync();
+                    ClientObject client = new ClientObject(tcpClient, this);
+                    clients.Add(client);
+                    _ = Task.Run(client.ProcessAsync);
+                }
+            }
+            catch (Exception ex)
+            {
+                dispatcher.Invoke(() => main.Chat.Items.Add(ex.ToString()));
+            }
+            finally { Disconnect(); }
+        }
+        protected internal async Task BroadcastMessageAsync(string message, string id)
+        {
+            foreach (ClientObject client in clients)
+            {
+                if (client.Id != id)
+                {
+                    await client.Writer.WriteLineAsync(message);
+                    await client.Writer.FlushAsync();
+                }
+            }
+        }
+        protected internal void Disconnect()
+        {
+            foreach (ClientObject client in clients)
+            {
+                client.Close();
+            }
+            tcplistener.Stop();
+        }
+    }
+
+    public class ClientObject
+    {
+        protected internal string Id { get; } = Guid.NewGuid().ToString();
+        protected internal StreamWriter Writer { get; }
+        protected internal StreamReader Reader { get; }
+
+        TcpClient client;
+        ServerObject serverObject;
+
+        public ClientObject(TcpClient tcpClient, ServerObject server)
+        {
+            client = tcpClient;
+            serverObject = server;
+            var stream = client.GetStream();
+            Writer = new StreamWriter(stream);
+            Reader = new StreamReader(stream);
+        }
+
+        public async Task ProcessAsync()
+        {
+            try
+            {
+                string? userName = await Reader.ReadLineAsync();
+                string? message = $"{userName} вошел в чат";
+                await serverObject.BroadcastMessageAsync(message, Id);
+                serverObject.dispatcher.Invoke(() => serverObject.main.Chat.Items.Add(message));
+                while (true)
+                {
+                    try
+                    {
+                        message = await Reader.ReadLineAsync();
+                        if (message == null) continue;
+                        message = $"{userName}: {message}";
+                        serverObject.dispatcher.Invoke(() => serverObject.main.Chat.Items.Add(message));
+                        await serverObject.BroadcastMessageAsync(message, Id);
+                    }
+                    catch
+                    {
+                        message = $"{userName} покинул чат";
+                        serverObject.dispatcher.Invoke(() => serverObject.main.Chat.Items.Add(message));
+                        await serverObject.BroadcastMessageAsync(message, Id);
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                serverObject.dispatcher.Invoke(() => serverObject.main.Chat.Items.Add(ex.Message));
+            }
+            finally
+            {
+                serverObject.RemoveConnetion(Id);
+            }
+        }
+
+        protected internal void Close()
+        {
+            client?.Close();
+            Writer?.Close();
+            Reader?.Close();
+        }
+    }
+
+}
+
+
---
+++ ChatClient/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows;
+
+namespace ChatClient
+{
+
+    public partial class MainWindow : Window
+    {
+        IPEndPoint iPEnd;
+        TcpClient client = null;
+        StreamReader? reader = null;
+        StreamWriter? writer = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void SendAnswer_Click(object sender, RoutedEventArgs e)
+        {
+            if (stringAnswer.Text == string.Empty && stringAnswer.Text == "Введите сообщение")
+            {
+                Chat.Items.Add("Введите сообщение!!!");
+                return;
+            }
+            await SendMessageAsync(writer, stringAnswer.Text);
+            Chat.Items.Add($"Вы отправили {stringAnswer.Text}");
+            stringAnswer.Text = "Введите сообщение";
+
+        }
+        private void stringAnswer_IsKeyboardFocusWithinChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (stringAnswer.Text == "Введите сообщение") stringAnswer.Text = string.Empty;
+        }
+        private void stringAnswer_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (stringAnswer.Text == string.Empty) stringAnswer.Text = "Введите сообщение";
+        }
+
+        private void Connnect_Click(object sender, RoutedEventArgs e)
+        {
+            if (LoginChat.Text == string.Empty) { Chat.Items.Add("Введите имя"); return; }
+
+            try
+            {
+                client = new TcpClient();
+                string host = "127.0.0.1";
+                int port = 888;
+                iPEnd = new IPEndPoint(IPAddress.Parse(host), port);
+                client.Connect(iPEnd);
+                reader = new StreamReader(client.GetStream());
+                writer = new StreamWriter(client.GetStream());
+                if (reader is null || writer is null) return;
+                writer.WriteLineAsync(LoginChat.Text);
+                writer.FlushAsync();
+
+                Task.Run(() => ResiveMessageAsync(reader));
+                Dispatcher.Invoke(() => Chat.Items.Add("Соединение успешно"));
+            }
+            catch (Exception ex)
+            {
+                Dispatcher.Invoke(() => Chat.Items.Add(ex.Message));
+            }
+        }
+
+        private void Disconnect_Click(object sender, RoutedEventArgs e)
+        {
+            client?.Close();
+            writer?.Close();
+            reader?.Close();
+        }
+
+        private async Task ResiveMessageAsync(StreamReader reader)
+        {
+            while (true)
+            {
+                try
+                {
+                    string? message = await reader.ReadLineAsync();
+                    if (string.IsNullOrEmpty(message)) continue;
+                    Dispatcher.Invoke(() => Chat.Items.Add(message));
+                }
+                catch
+                {
+                    break;
+                }
+            }
+        }
+
+        private async Task SendMessageAsync(StreamWriter writer, string message)
+        {
+            await writer.WriteLineAsync(message);
+            await writer.FlushAsync();
+        }
+    }
+
+
+}
